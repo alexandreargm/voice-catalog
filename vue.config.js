bcf169e159c8130adea8f952a373af154490c942
alexandreargm <alexandreargm@gmail.com>
2021-08-02 20:53:47 +0200

    Fixed publicPath for production

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,7 @@
-const path = require('path')
+// const path = require('path')
 
 module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias: {
-        images: path.resolve(__dirname, 'src/assets/images')
-      }
-    }
-  },
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
   css: {
     // ref: https://cli.vuejs.org/guide/css.html#passing-options-to-pre-processor-loaders
     loaderOptions: {
