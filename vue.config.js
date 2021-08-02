844e0a7f030df327bb45636b03ff3f92dadaba62
alexandreargm <alexandreargm@gmail.com>
2021-08-02 22:37:34 +0200

    Added comment about publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 // const path = require('path')
 
 module.exports = {
+  // Fix routes start with '/' on prod. Ref.: https://cli.vuejs.org/config/#publicpath
   publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
   css: {
     // ref: https://cli.vuejs.org/guide/css.html#passing-options-to-pre-processor-loaders
