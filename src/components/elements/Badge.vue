6a41f76bda0271b903c4c6d375301698e90f6a95
alexandreargm <alexandreargm@gmail.com>
2021-08-03 22:34:27 +0200

    Created Badge.vue element

---
+++ src/components/elements/Badge.vue
@@ -0,0 +1,25 @@
+<template>
+  <span class="badge" :class="variant">
+    <slot />
+  </span>
+</template>
+
+<script>
+import { variant } from '@/mixins/component-utils.js'
+
+export default {
+  mixins: [variant]
+}
+</script>
+
+<style lang="scss">
+.badge {
+  height: 32px;
+  padding: 4px;
+  width: 32px;
+}
+
+.badge.is-rounded {
+  border-radius: 9999px;
+}
+</style>
