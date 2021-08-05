38272d9c5b0bcd0bf6c2de9fefb706ced2d68c15
alexandreargm <alexandreargm@gmail.com>
2021-08-06 01:11:03 +0200

    Added Favorite drawer state

--- src/store/modules/favorites.js
+++ src/store/modules/favorites.js
@@ -1,30 +1,43 @@
 const state = {
-  ids: []
+  ids: [],
+  isDrawerOpen: false
 }
 
 const getters = {
-  getFavorites: state => {
+  getFavorites (state) {
     return state.ids
   },
-  getFavoriteById: state => id => {
-    return state.ids.find(favoriteId => favoriteId === id)
+  isFavorite (state) {
+    return id => state.ids.find(favoriteId => favoriteId === id) !== undefined
+  },
+  isDrawerOpen (state) {
+    return state.isDrawerOpen
   }
 }
 
 const actions = {
   addVoiceToFavorites ({ commit, getters }, voice) {
     const { id } = voice
-    const isAlreadyFavorite = getters.getFavoriteById(id)
+    const isAlreadyFavorite = getters.isFavorite(id)
     if (isAlreadyFavorite) return false
 
     commit('ADD_FAVORITE', id)
     return true
+  },
+  toggleDrawer ({ commit, getters }) {
+    const isOpen = getters.isDrawerOpen
+    const newState = !isOpen
+
+    commit('SET_IS_DRAWER_OPEN', newState)
   }
 }
 
 const mutations = {
   ADD_FAVORITE (state, voiceId) {
     state.ids.push(voiceId)
+  },
+  SET_IS_DRAWER_OPEN (state, isOpen) {
+    state.isDrawerOpen = isOpen
   }
 }
 
