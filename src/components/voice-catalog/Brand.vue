054eb84b6bec8362cc8cb1c1d01d63bebb6eaf57
alexandreargm <alexandreargm@gmail.com>
2021-08-06 01:12:47 +0200

    Created brand component

---
+++ src/components/voice-catalog/Brand.vue
@@ -0,0 +1,14 @@
+<template>
+  <a href="#" class="brand">
+    Voice catalog
+  </a>
+</template>
+
+<style lang="scss">
+.brand {
+  color: $brand;
+  font-size: $font-2xl;
+  font-weight: $weight-black;
+  text-decoration: none;
+}
+</style>
