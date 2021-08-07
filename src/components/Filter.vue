9e2e8f81a4f63dc18e8a2600c4d290f6be7c456d
alexandreargm <alexandreargm@gmail.com>
2021-08-07 15:38:04 +0200

    Created Filter component

---
+++ src/components/Filter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="filter">
+    <div class="filter__inputs">
+      <slot name="inputs" />
+    </div>
+
+    <div class="filter__params_display">
+      <slot name="params" />
+    </div>
+
+    <div class="filter__params_display">
+      <slot name="display" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  /* ad */
+</style>
