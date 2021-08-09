6da9a27e32a9d260e850a7e780c6bc31b32bea45
alexandreargm <alexandreargm@gmail.com>
2021-08-09 20:40:22 +0200

    Created random button

---
+++ src/components/voice-catalog/RandomButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <Button class="random-button__" :variant="['is-secondary', 'is-compact']">
+    <SparklesIcon class="random-button__icon" />
+
+    <span class="random-button__text"> Random</span>
+  </Button>
+</template>
+
+<script>
+import Button from '@/components/elements/Button.vue'
+import { SparklesIcon } from '@vue-hero-icons/solid'
+export default {
+  components: {
+    Button,
+    SparklesIcon
+  }
+}
+</script>
+
+<style lang="scss">
+.random-button {
+  &__text {
+    display: none;
+    margin-left: $spacing-sm;
+
+    @include breakpoint('tablet') {
+      display: block;
+    }
+  }
+}
+</style>
