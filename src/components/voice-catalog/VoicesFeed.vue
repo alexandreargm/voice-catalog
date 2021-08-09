2af7bb421ec8f99e92e57c1d648320c28125c77b
alexandreargm <alexandreargm@gmail.com>
2021-08-09 18:29:56 +0200

    Merged voicesFeedGrid into VoicesFeed to prevent over engineering

--- src/components/voice-catalog/VoicesFeed.vue
+++ src/components/voice-catalog/VoicesFeed.vue
@@ -1,15 +1,58 @@
 <template>
   <section class="voices-feed">
-    <voices-feed-filters />
+    <voice-searchbar v-model="searchOptions" @submit="handleFeedSearch" class="voices-feed__searchbar" />
 
-    <voices-feed-grid />
+    <grid class="voices-feed__grid">
+      <voice-card
+        v-for="voice in getVoices"
+        :key="voice.id"
+        :id="voice.id"
+        :name="voice.name"
+        :icon="voice.icon"
+        :tags="voice.tags"
+      />
+    </grid>
   </section>
 </template>
 
 <script>
-import VoicesFeedFilters from '@/components/voice-catalog/VoicesFeedFilters.vue'
-import VoicesFeedGrid from '@/components/voice-catalog/VoicesFeedGrid.vue'
+import VoiceSearchbar from './VoiceSearchbar.vue'
+import Grid from '@/components/generic/Grid.vue'
+import VoiceCard from '@/components/voice-catalog/VoiceCard.vue'
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
-  components: { VoicesFeedFilters, VoicesFeedGrid }
+  components: {
+    VoiceCard,
+    Grid,
+    VoiceSearchbar
+  },
+  data () {
+    return {
+      searchOptions: {}
+    }
+  },
+  computed: {
+    ...mapGetters('voices', [
+      'getVoices'
+    ])
+  },
+  methods: {
+    handleFeedSearch () {
+      console.log('searching feed')
+    },
+    ...mapActions('voices', [
+      'fetchVoices'
+    ])
+  },
+  created () {
+    this.fetchVoices()
+  }
 }
 </script>
+
+<style lang="scss">
+.voices-feed {
+  padding: $padding;
+}
+</style>
