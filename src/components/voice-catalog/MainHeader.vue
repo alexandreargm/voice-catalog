7980ac6bf3d921e56111124136a12b5b8eaaf03e
alexandreargm <alexandreargm@gmail.com>
2021-08-10 10:57:00 +0200

    Favorites button is no longer secondary variant

--- src/components/voice-catalog/MainHeader.vue
+++ src/components/voice-catalog/MainHeader.vue
@@ -5,7 +5,7 @@
 
     <brand />
 
-    <Button :variant="['is-secondary', 'is-compact']" class="toggle-drawer" @click="toggleFavoriteDrawer">
+    <Button :variant="['is-compact']" class="toggle-drawer" @click="toggleFavoriteDrawer">
       <ViewGridAddIcon size="24" class="toggle-drawer__icon icon" />
 
       <span class="toggle-drawer__text">
