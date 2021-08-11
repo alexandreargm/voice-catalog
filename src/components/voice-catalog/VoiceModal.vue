ceace7832327215dda8d5c18525c7fe07a802878
alexandreargm <alexandreargm@gmail.com>
2021-08-11 23:39:03 +0200

    Created voice modal composite

---
+++ src/components/voice-catalog/VoiceModal.vue
@@ -0,0 +1,28 @@
+<template>
+  <modal :is-open="isOpen" @close-modal="$emit('close-modal')" class="voice-modal">
+    <div class="voice-modal__body">
+      <slot />
+    </div>
+  </modal>
+</template>
+
+<script>
+import Modal from '@/components/generic/Modal.vue'
+export default {
+  components: { Modal },
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.voice-modal {
+  &__body {
+    padding: $spacing $spacing-2xl;
+  }
+}
+</style>
