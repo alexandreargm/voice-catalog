374a3456abc1d91ff6fe8538d62f6083cbc9e786
alexandreargm <alexandreargm@gmail.com>
2021-08-12 00:33:24 +0200

    Added modal footer slot

--- src/components/voice-catalog/VoiceModal.vue
+++ src/components/voice-catalog/VoiceModal.vue
@@ -3,13 +3,21 @@
     <div class="voice-modal__body">
       <slot />
     </div>
+
+    <template #footer>
+      <random-button @click="$emit('random-button')" class="voice-modal__random-button" />
+    </template>
   </modal>
 </template>
 
 <script>
 import Modal from '@/components/generic/Modal.vue'
+import RandomButton from '@/components/voice-catalog/RandomButton'
 export default {
-  components: { Modal },
+  components: {
+    Modal,
+    RandomButton
+  },
   props: {
     isOpen: {
       type: Boolean,
@@ -23,6 +31,10 @@ export default {
 .voice-modal {
   &__body {
     padding: $spacing $spacing-2xl;
+  }
+
+  &__random-button {
+    margin-top: $spacing;
   }
 }
 </style>
