9baa81a83130b1a5b7fe9def30c1a8641d010a30
alexandreargm <alexandreargm@gmail.com>
2021-08-12 00:31:48 +0200

    Added hide xs variant

--- src/components/voice-catalog/RandomButton.vue
+++ src/components/voice-catalog/RandomButton.vue
@@ -1,15 +1,19 @@
 <template>
-  <Button class="random-button__" :variant="['is-secondary', 'is-compact']" type="button" @click="$emit('click')">
+  <Button class="random-button" :variant="['is-secondary', 'is-compact']" type="button" @click="$emit('click')" :class="variant">
     <RefreshIcon class="random-button__icon" />
 
-    <span class="random-button__text"> Random</span>
+    <span class="random-button__text">
+      Random
+    </span>
   </Button>
 </template>
 
 <script>
+import { variant } from '@/mixins/component-utils'
 import Button from '@/components/elements/Button.vue'
 import { RefreshIcon } from '@vue-hero-icons/solid'
 export default {
+  mixins: [variant],
   components: {
     Button,
     RefreshIcon
@@ -20,8 +24,13 @@ export default {
 <style lang="scss">
 .random-button {
   &__text {
-    display: none;
     margin-left: $spacing-sm;
+  }
+}
+
+.random-button.hide-xs {
+  .random-button__text {
+    display: none;
 
     @include breakpoint('mobile2') {
       display: block;
