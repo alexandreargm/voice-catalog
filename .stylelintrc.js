07abcf08d3fc9fcde993abd42dc3c804bc979101
alexandreargm <alexandreargm@gmail.com>
2021-08-12 02:47:59 +0200

    Changed double quotes for single quotes

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,20 +1,19 @@
 module.exports = {
-    extends: [
-      "stylelint-config-recommended",
-      "stylelint-config-recommended-scss",
-      "stylelint-config-sass-guidelines"
-    ],
-    plugins: [
-      "stylelint-order",
-      "stylelint-scss"
-    ],
-    rules: {  
-      // order rules
-      "order/properties-alphabetical-order": true,
-      // Scss recommended rules
-      "at-rule-no-unknown": null,
-      "scss/at-rule-no-unknown": true,
-      "selector-class-pattern": null
-    }
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-sass-guidelines'
+  ],
+  plugins: [
+    'stylelint-order',
+    'stylelint-scss'
+  ],
+  rules: {
+    // order rules
+    'order/properties-alphabetical-order': true,
+    // Scss recommended rules
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'selector-class-pattern': null
   }
-  
+}
